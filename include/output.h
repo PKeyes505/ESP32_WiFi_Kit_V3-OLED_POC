d8b8f33e72d2994bfe28c389123aa0a590a6ddeb
PKeyes505 <61755459+PKeyes505@users.noreply.github.com>
2023-07-06 16:00:46 -0600

    Added include/output.h

---
+++ include/output.h
@@ -0,0 +1,37 @@
+#include <Arduino.h>
+#include <Wire.h>
+#include "heltec.h"
+
+enum OutputType {
+  CONSOLE,
+  OLED,
+  LOG
+};
+
+class Output {
+  public:
+    Output(SSD1306Wire* display) {
+      _display = display;
+    }
+
+    void print(String message, OutputType type) {
+      switch (type) {
+        case CONSOLE:
+          Serial.println(message);
+          break;
+        case OLED:
+          _display->clear();
+          _display->drawString(0, 0, message);
+          _display->display();
+          break;
+        case LOG:
+          // Log the message to a file or other storage
+          break;
+        default:
+          break;
+      }
+    }
+
+  private:
+    SSD1306Wire* _display;
+};
